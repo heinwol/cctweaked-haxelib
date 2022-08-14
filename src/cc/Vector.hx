d01efa07b0eba4e5d87cafeb0da3454bfa90ea93
jf908 <josh@thefindons.com>
2022-08-14 20:58:21 +0100

    Add missing vector functions

--- src/cc/Vector.hx
+++ src/cc/Vector.hx
@@ -7,10 +7,13 @@ extern class Vector {
 	public function add(other:Vector):Vector;
 	public function sub(other:Vector):Vector;
 	public function mul(n:Float):Vector;
+	public function div(n:Float):Vector;
+	public function unm():Vector;
 	public function dot(other:Vector):Float;
 	public function cross(other:Vector):Float;
 	public function length():Float;
 	public function normalize():Vector;
 	public function round():Vector;
 	@:native("tostring") public function toString():String;
+	public function equals(other:Vector):Bool;
 }
